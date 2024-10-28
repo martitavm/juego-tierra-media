c5a7a3502458c5c916bee4eaf60ab32a35df7cc4
Ana Morillo Troya <160231626+AnitaMT@users.noreply.github.com>
2024-10-28 08:59:57 +0100

    Delete equipar_arma_personaje.py

--- equipar_arma_personaje.py
+++
@@ -1,4 +0,0 @@
-# Equipar un arma a un personaje:
-
-# Permite que un personaje seleccione un arma de su inventario de equipamiento y la equipe.
-# Si el personaje ya tiene una arma equipada, se debe reemplazar con la nueva.
